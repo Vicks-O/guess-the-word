76f5cebe6b89edc1c3c28af649303895dbcdd798
vicks_o <avthienp@gmail.com>
2023-02-27 13:13:47 +0400

    display guessed word

--- js/script.js
+++ js/script.js
@@ -67,5 +67,40 @@ const validateInput = function (input){
     } else {
     guessedLetters.push(guess);
     console.log(guessedLetters);
+
   }
 };
+const showGuessedLetters = function(){
+  //Clear the list first
+  guessedLettersElement.innerHTML="";
+  for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText=letter;
+    guessedLettersElement.append(li);
+  }
+};
+const updateWordInProgress = function (guessedLetters) {
+  const wordUpper = word.toUpperCase();
+  const wordArray = wordUpper.split("");
+  const revealWord = [];
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+      revealWord.push(letter.toUpperCase());
+    } else {
+      revealWord.push("☀️");
+      revealWord.push("●");
+    }
+  }
+  // console.log(revealWord);
+  wordInProgress.innerText = revealWord.join("");
+  checkIfWin();
+};
+const checkIfWin = function () {
+  if (word.toUpperCase() === wordInProgress.innerText) {
+    message.classList.add("win");
+    message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+  }
+};
+
+  }
+}
